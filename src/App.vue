f215e6ffaab86953189eddd19130db88dc90d4b3
yashar-cyber <58699677+yashar-cyber@users.noreply.github.com>
2021-06-28 04:23:59 +0430

    created the-header and inner tags

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-
+  <admin-page></admin-page>
 </template>
 
 <script>
 
+import AdminPage from "@/components/Admin/AdminPage";
 export default {
   name: 'App',
-
+  components: {AdminPage},
   data: () => ({
     //
   }),
--- src/components/Common/TheHeader.vue
+++ src/components/Common/TheHeader.vue
@@ -1,6 +1,15 @@
 <template>
-  //for navigations
-  <slot></slot>
+  <div id="header-container">
+    <div id="header-list">list</div>
+    <div id="header-profile>">
+      <div id="profile-content">
+        <div id="content-dropdown"></div>
+        <div id="content-info"></div>
+        <div id="content-image"></div>
+      </div>
+      <div id="profile-menu"></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,6 +18,18 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
+#header-container {
+  width: 65%;
+  height: 10%;
+  border: 1px solid black;
+  border-radius: 50px;
+  padding: 0 30px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+}
 
 </style>
