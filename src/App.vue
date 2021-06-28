1387c94eb4b4dfc8b7dac929c35bf28ca0762cfb
yashar-cyber <58699677+yashar-cyber@users.noreply.github.com>
2021-06-29 01:42:59 +0430

    nothing special yet.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <router-view></router-view>
+<!--  <router-view></router-view>-->
+  <admin-page></admin-page>
 </template>
 
 <script>
 
+import AdminPage from "@/components/Admin/AdminPage";
 export default {
   name: 'App',
-
+  components: {AdminPage},
   data: () => ({
     //
   }),
