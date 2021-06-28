5bdda9864faa10502f4dd10af7427680794aa580
yashar-cyber <58699677+yashar-cyber@users.noreply.github.com>
2021-06-28 05:21:48 +0430

    added the header profile tags

--- src/components/Common/TheHeader.vue
+++ src/components/Common/TheHeader.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="header-container">
+
     <div id="header-list">list</div>
-    <div id="header-profile>">
+
+    <div id="header-profile">
+
       <div id="profile-content">
-        <div id="content-dropdown"></div>
-        <div id="content-info"></div>
-        <div id="content-image"></div>
+        <div id="content-dropdown">🔽</div>
+        <div id="content-info">
+          <label id="info-name">Jafar Tanha</label>
+          <label id="info-ID">123456789</label>
+        </div>
+        <div id="content-image">
+          pic
+        </div>
       </div>
+
       <div id="profile-menu"></div>
+
     </div>
+
   </div>
 </template>
 
@@ -30,6 +41,42 @@ export default {
   flex-flow: row nowrap;
   justify-content: space-between;
   align-items: center;
+
+  #header-profile {
+
+    #profile-content {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: center;
+      align-items: center;
+
+      #content-dropdown {
+        margin: 0 8px;
+      }
+      #content-info {
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        align-items: flex-end;
+        margin: 0 8px;
+
+        #info-name {
+          font-size: 15pt;
+        }
+        #info-ID {
+          font-size: 11pt;
+        }
+      }
+      #content-image {
+
+        border-radius: 50%;
+        background: gray;
+        margin: 0 8px;
+      }
+    }
+  }
 }
+
+
 
 </style>
