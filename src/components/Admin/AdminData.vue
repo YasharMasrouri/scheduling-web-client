5e232bd0dd4470c063f3dce97374888fe9e0cc28
yashar-cyber <58699677+yashar-cyber@users.noreply.github.com>
2021-07-04 23:38:24 +0430

    edited backdrop, created admin-data page

--- src/components/Admin/AdminData.vue
+++ src/components/Admin/AdminData.vue
@@ -1,17 +1,33 @@
 <template>
-  <the-container>
-
-  </the-container>
+  <div id="admin-data-container">
+    <div id="admin-data-backdrop"></div>
+    <div id="admin-data-image"></div>
+  </div>
 </template>
 
 <script>
-import TheContainer from "../Common/TheContainer";
 export default {
   name: "AdminData",
-  components: {TheContainer}
+  components: {}
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "./../../../public/-variables";
 
+#admin-data-container {
+  width: 100%;
+  height: 100%;
+  @include flex-center;
+
+  #admin-data-backdrop {
+    display: none;
+    @include page-backdrop;
+  }
+
+  #admin-data-image {
+    background-image: url("./../../assets/AdminDatas.svg");
+    @include page-image;
+  }
+}
 </style>
