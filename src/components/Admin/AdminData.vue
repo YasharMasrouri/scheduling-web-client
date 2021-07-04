e997ef04416b905a3cb8734728b72f6e09e25a74
yashar-cyber <58699677+yashar-cyber@users.noreply.github.com>
2021-07-05 00:25:11 +0430

    amin data columns created, and data-items created.

--- src/components/Admin/AdminData.vue
+++ src/components/Admin/AdminData.vue
@@ -1,14 +1,42 @@
 <template>
   <div id="admin-data-container">
+
+    <div class="admin-data-column" id="admin-data-bells">
+      <h3>Bells</h3>
+      <div class="admin-data-column-content">
+        <data-item></data-item>
+        <data-item></data-item>
+      </div>
+      <button class="admin-data-add-btn">Add</button>
+    </div>
+    <div class="admin-data-column" id="admin-data-day">
+      <h3>Day</h3>
+      <div class="admin-data-column-content">
+        <data-item></data-item>
+        <data-item></data-item>
+      </div>
+      <button class="admin-data-add-btn">Add</button>
+    </div>
+    <div class="admin-data-column" id="admin-data-courses">
+      <h3>Courses</h3>
+      <div class="admin-data-column-content">
+        <data-item></data-item>
+        <data-item></data-item>
+      </div>
+      <button class="admin-data-add-btn">Add</button>
+    </div>
+    <button id="admin-data-generate">Generate Schedule</button>
+
     <div id="admin-data-backdrop"></div>
     <div id="admin-data-image"></div>
   </div>
 </template>
 
 <script>
+import DataItem from "@/components/Admin/DataItem";
 export default {
   name: "AdminData",
-  components: {}
+  components: {DataItem}
 }
 </script>
 
@@ -18,13 +46,61 @@ export default {
 #admin-data-container {
   width: 100%;
   height: 100%;
-  @include flex-center;
+  padding: 32px 64px;
+  display: grid;
+  grid-template-areas:
+  'bells day courses'
+  'empty empty generate';
+  grid-template-rows: 9fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  .admin-data-column {
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-rows: 0.7fr 6.3fr 1fr;
+    text-align: center;
+
+    .admin-data-column-content {
+      height: 100%;
+      width: 100%;
+      padding: 8px;
+    }
+    .admin-data-add-btn {
+      background: $color-orange-main;
+      color: white;
+      border-radius: 30px;
+      margin: 12px 84px;
+    }
+    .admin-data-add-btn:hover {
+      background: lighten($color-orange-main,5%);
+    }
+  }
+
+  #admin-data-bells {
+    grid-area: bells;
+  }
+  #admin-data-day {
+    grid-area: day;
+  }
+  #admin-data-courses {
+    grid-area: courses;
+  }
+  #admin-data-generate {
+    grid-area: generate;
+    background: $color-blue-btn;
+    color: white;
+    border-radius: 30px;
+    margin: 16px 64px 0;
+  }
+  #admin-data-generate:hover {
+    background: lighten($color-blue-btn,5%);
+  }
 
   #admin-data-backdrop {
     display: none;
     @include page-backdrop;
   }
-
   #admin-data-image {
     background-image: url("./../../assets/AdminDatas.svg");
     @include page-image;
---
+++ src/components/Admin/DataItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="data-item-container">
+    <div id="data-item-edit"></div>
+    <div id="data-item-delete"></div>
+    <div id="data-item-box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataItem"
+}
+</script>
+
+<style scoped lang="scss">
+@import "./../../../public/-variables";
+#data-item-container {
+  height: 140px;
+  border: 1px solid black;
+  margin: 32px;
+
+  #data-item-edit {
+    width: 45px;
+    height: 45px;
+    background: $color-orange-main;
+    border-radius: 50%;
+  }
+  #data-item-delete {
+    width: 45px;
+    height: 45px;
+    background: $color-blue-btn;
+    border-radius: 50%;
+  }
+}
+</style>
