3a6ebc2ee519948723f598e6003e7f1884f19c10
Bita <bitakhashechian00@gmail.com>
2021-07-08 10:37:05 -0700

    avatar design changed...

--- src/components/Common/EditProfile.vue
+++ src/components/Common/EditProfile.vue
@@ -1,35 +1,36 @@
 <template>
   <div id="editProfile-container">
     <div class="text-center">
-    <v-card flat class="ma-3">
+      <v-card flat class="ma-3">
 
-      <v-responsive>
-        <div class="avatar">
-        <v-avatar class="my-4" size="200">
-          <img src="https://picsum.photos/id/11/500/300" />
-        </v-avatar>
+        <v-responsive>
+          <div class="imgDiv">
+            <v-avatar id="avatar" class="my-4" size="200">
+              <img src="https://picsum.photos/id/11/500/300"/>
+              <input type="file" id="file">
+              <label for="file" id="uploadBtn">
 
-        </div>
-      </v-responsive>
+                <v-icon color="white">mdi-camera</v-icon>
+              </label>
+            </v-avatar>
 
-      <v-card-text >
-        <div class="text-center display-1 black--text mt-1">Name: Jafar Tanha</div>
-        <div class="text-center headline black--text mt-1">Code:12345678</div>
-        <div class="text-center headline black--text my-1">Role: Master</div>
-      </v-card-text>
+          </div>
+        </v-responsive>
 
-      <v-card-action >
-<div class="editBtns">
-        <EditName />
-        <EditPassword />
-</div>
-      </v-card-action>
-      <v-file-input
-          color="#ED6038"
-          label="Pick Avatar"
-          prepend-icon="mdi-camera"
-      ></v-file-input>
-    </v-card>
+        <v-card-text>
+          <div class="text-center display-1 black--text mt-1">Name: Jafar Tanha</div>
+          <div class="text-center headline black--text mt-1">Code:12345678</div>
+          <div class="text-center headline black--text my-1">Role: Master</div>
+        </v-card-text>
+
+        <v-card-action>
+          <div class="editBtns">
+            <EditName/>
+            <EditPassword/>
+          </div>
+        </v-card-action>
+
+      </v-card>
     </div>
   </div>
 </template>
@@ -37,6 +38,7 @@
 <script>
 import EditName from "./EditName";
 import EditPassword from "./EditPassword";
+
 export default {
   name: "EditProfile",
   components: {
@@ -48,10 +50,30 @@ export default {
 
 <style scoped lang="scss">
 @import "./../../../public/-variables";
+
 #editProfile-container {
   width: 100%;
   height: 100%;
   @include flex-center;
+
+  #avatar {
+    border: 1px solid grey;
+  }
+
+  #file {
+    display: none;
+  }
+
+  #uploadBtn {
+    width: 100%;
+    height: 40px;
+    bottom: 0;
+    position: absolute;
+    background: rgba(0, 0, 0, 0.5);
+    color: wheat;
+    line-height: 30px;
+    cursor: pointer;
+  }
 
   .text-center {
     display: flex;
@@ -60,7 +82,7 @@ export default {
     width: 25vw;
   }
 
-  .avatar {
+  .imgDiv {
     display: flex;
     justify-content: center;
   }
