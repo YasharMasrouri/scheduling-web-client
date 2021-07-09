8df96dd4bb2467c920e63bb835805d79813a3eab
Mahdi <mahdichavoshi14@gmail.com>
2021-07-10 01:12:14 +0430

    admin home and header finished without fetch

--- src/components/Admin/AdminPage.vue
+++ src/components/Admin/AdminPage.vue
@@ -1,21 +1,14 @@
 <template>
   <div id="admin-page-container">
     <the-header>
-      <router-link class="the-header-list-item list-item-active" to="/Admin/Home">Home</router-link>
+      <router-link class="the-header-list-item" to="/Admin/Home">Home</router-link>
       <router-link class="the-header-list-item" to="/Admin/Data">Data</router-link>
       <router-link class="the-header-list-item" to="/Admin/Students">Students</router-link>
       <router-link class="the-header-list-item" to="/Admin/Masters">Masters</router-link>
       <router-link class="the-header-list-item" to="/Admin/Announcements">Announcements</router-link>
     </the-header>
-<!--    <router-view></router-view>-->
     <the-container>
-<!--      <router-view></router-view>-->
-<!--      <admin-home></admin-home>-->
-<!--      <admin-data></admin-data>-->
-<!--      <admin-students></admin-students>-->
-<!--      <admin-masters></admin-masters>-->
-<!--      <m-a-announcements></m-a-announcements>-->
-      <edit-profile></edit-profile>
+      <router-view></router-view>
     </the-container>
 
   </div>
@@ -25,10 +18,9 @@
 <script>
 import TheHeader from "../Common/TheHeader";
 import TheContainer from "@/components/Common/TheContainer";
-import EditProfile from "@/components/Common/EditProfile";
 export default {
   name: "AdminPage",
-  components: {EditProfile, TheContainer, TheHeader}
+  components: { TheContainer, TheHeader}
 }
 </script>
 
@@ -38,5 +30,9 @@ export default {
 #admin-page-container {
   @include page-container;
 
+  #admin-page-image {
+    background-image: url("./../../assets/StudentHome.svg");
+    @include page-image;
+  }
 }
 </style>
